6b02a4cd80961f27c2b4dc67412fcf245334c774
alessandro <alessandro.appolloni@libero.it>
2018-04-05 12:56:14 +0200

    Aggiunta gestione creazione template html

--- start.c
+++ start.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+	
+	FILE *fd;
 
 	char cnt_n_args = 0;
 
 	int *ptr_n_args;
 
+	char **ptr_argv;
+
 	ptr_n_args = &argc;
 
+	ptr_argv = argv;
+
 	puts("R55-Versione-001");
+
+	puts("Template HTML");
+
+	puts("Sintassi: ./start <filename> <title> <body>");
 
 	printf("Numero parametri ricevuti: %d\n", *ptr_n_args);
 
 	while (*argv!='\0') {
-		cnt_n_args++;
 		printf("Parametro [%d]: %s\n", cnt_n_args, *argv++);
+		cnt_n_args++;
 	}
 
+	*ptr_argv++;
+
+	fd = fopen(*ptr_argv, "w");
+
+	*ptr_argv++;
+
+	fprintf(fd, "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset=\"UTF-8\">\n\t\t<title>%s</title>\n\t</head>\n\n\t<body>\n\t\t", *ptr_argv);
+
+	*ptr_argv++;
+
+	fprintf(fd, "%s\n\t</body>\n\n</html>\n\n", *ptr_argv);
+
+	fclose(fd);
+
+  return 0;
 }
+
+
+
