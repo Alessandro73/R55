97efbbe868ea679df262b4839ea72652b39a2735
Alessandro73 <alessandro.appolloni@libero.it>
2018-04-05 11:01:01 +0200

    Aggiunto Ciclo Parametri
    Aggiunto Ciclo per la visualizzazione dei parametri passati e variabile contatore numero parametri

--- start.c
+++ start.c
@@ -2,10 +2,17 @@
 
 int main(int argc, char *argv[]) {
 
+	char cnt_n_args = 0;
+
 	int *ptr_n_args;
 
 	ptr_n_args = &argc;
 
 	printf("Numero parametri ricevuti: %d\n", *ptr_n_args);
+
+	while (*argv!='\0') {
+		cnt_n_args++;
+		printf("Parametro [%d]: %s\n", cnt_n_args, *argv++);
+	}
 
 }
