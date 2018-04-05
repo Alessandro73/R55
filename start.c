536a539a66cad7dacd2d3816f681623ea7fbe890
alessandro <alessandro.appolloni@libero.it>
2018-04-05 13:31:39 +0200

    Eliminata gestione template HTML

--- start.c
+++ start.c
@@ -16,10 +16,6 @@ int main(int argc, char *argv[]) {
 
 	puts("R55-Versione-001");
 
-	puts("Template HTML");
-
-	puts("Sintassi: ./start <filename> <title> <body>");
-
 	printf("Numero parametri ricevuti: %d\n", *ptr_n_args);
 
 	while (*argv!='\0') {
@@ -27,21 +23,14 @@ int main(int argc, char *argv[]) {
 		cnt_n_args++;
 	}
 
-	*ptr_argv++;
+	cnt_n_args = 0;
 
-	fd = fopen(*ptr_argv, "w");
+	while (*ptr_argv!='\0') {
+		printf("Parametro [%d]: %s\n", cnt_n_args, *ptr_argv++);
+		cnt_n_args++;
+	}
 
-	*ptr_argv++;
-
-	fprintf(fd, "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset=\"UTF-8\">\n\t\t<title>%s</title>\n\t</head>\n\n\t<body>\n\t\t", *ptr_argv);
-
-	*ptr_argv++;
-
-	fprintf(fd, "%s\n\t</body>\n\n</html>\n\n", *ptr_argv);
-
-	fclose(fd);
-
-  return 0;
+	return 0;
 }
 
 
